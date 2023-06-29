9e6252efb5476da4cf49aecfa2b5696c43aa0db4
EC2 Default User <ec2-user@ip-172-31-5-135.ap-northeast-1.compute.internal>
2023-06-29 13:10:19 +0000

    [Fix]public post

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,2 +1,39 @@
 class Public::PostsController < ApplicationController
+
+ def new
+   @post = Post.new
+ end
+
+  def create
+    @post = Post.new(post_params)
+    @post.end_user_user_id = current_end__user.id
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  def index
+    @posts = Post.page(params[:page])
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    @post_comment = PostComment.new
+  end
+
+  def destroy
+    post  = Post.find(params[:id])
+    post.destroy
+    redirect_to '/posts'
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:name, :image, :caption)
+  end
+
+
 end
