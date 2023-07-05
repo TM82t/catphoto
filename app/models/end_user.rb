d8b3dc9f80be8b05f5689d7fc282e5883ff06e83
EC2 Default User <ec2-user@ip-172-31-5-135.ap-northeast-1.compute.internal>
2023-07-05 13:03:05 +0000

    [Fix]end_user.rb to gest login

--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -15,23 +15,22 @@ class EndUser < ApplicationRecord
   has_many :followings, through: :follows, source: :follow
   has_many :followers, through: :reverse_of_follows, source: :follower
   # フォローしたときの処理
-  def follow(user_id)
-    follows.create(followed_id: user_id)
+  def follow(end_user_id)
+    follows.create(followed_id: end_user_id)
   end
   # フォローを外すときの処理
-  def unfollow(user_id)
-    follows.find_by(followed_id: user_id).destroy
+  def unfollow(end_user_id)
+    follows.find_by(followed_id: end_user_id).destroy
   end
   # フォローしているか判定
-  def following?(user)
-    followings.include?(user)
+  def following?(end_user)
+    followings.include?(end_user)
   end
 
   # ゲストログイン用
   def self.guest
-    find_or_create_by!(email: 'guest@example.com') do |user|
+    find_or_create_by!(end_user_name: 'ゲスト',email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
-      user.name = "ゲスト"
     end
   end
 
