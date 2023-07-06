f2d6fde4283ea0d1090b6dd5efddd064a830cc3f
EC2 Default User <ec2-user@ip-172-31-5-135.ap-northeast-1.compute.internal>
2023-07-06 13:34:52 +0000

    [Fix]post comment

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -1,10 +1,12 @@
 class Public::PostCommentsController < ApplicationController
+  before_action :ensure_correct_end_user, only: [:destroy]
 
   def create
-    post = Post.find(params[:post_id])
-    comment = current_end_user.post_comments.new(post_comment_params)
-    comment.post_id = post.id
-    comment.save
+    @post = Post.find(params[:post_id])
+    @comment = PostComment.new(post_comment_params)
+    @comment.end_user_id = current_end_user.id
+    @comment.post_id = @post.id
+    @comment.save
     redirect_to post_path(post)
   end
 
@@ -16,7 +18,14 @@ class Public::PostCommentsController < ApplicationController
   private
 
   def post_comment_params
-    params.require(:post_comment).permit(:comment)
+    params.require(:post_comment).permit(:comment, :post_id, :end_user_id)
+  end
+
+  def ensure_correct_end_user
+    @comment = Comment.find(params[:id])
+    unless @comment.end_user == current_end_user
+      redirect_to posts_path, notice: "投稿者以外コメントは削除できません。"
+    end
   end
 
 end
